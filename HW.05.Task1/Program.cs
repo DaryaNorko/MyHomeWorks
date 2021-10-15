11dc8b5ce6b22f1090544363130c420dfa23d166
Дарья Норко <d.i.norko@mail.ru>
2021-10-15 13:04:36 +0300

    Add Task1

---
+++ HW.05.Task1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace HW._05.Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] arrayRandom = new int [10];
+            Random rand = new Random();
+            for (int i = 0; i<arrayRandom.Length; i++)
+            {
+                arrayRandom[i] = rand.Next(0, 200);
+            }
+
+            int[] arrayUser = new int[arrayRandom.Length];
+            Console.WriteLine($"Please, input {arrayUser.Length} values.");
+            Console.WriteLine();
+            
+            for (int i = 0; i<arrayUser.Length; i++)
+            {
+                Console.Write($" Value number {i+1} - ");
+               int num = int.Parse(Console.ReadLine());
+                arrayUser[i] = num;
+            }
+            Console.WriteLine();
+
+            int[] arraySum = new int[arrayUser.Length];
+            for (int i = 0; i < arraySum.Length; i++)
+            {
+                arraySum[i] = arrayRandom[i] + arrayUser[i];
+            }
+
+            Console.WriteLine(" Array of random values: ");
+            for (int i = 0; i < arrayRandom.Length; i++)
+            {
+                Console.Write($"{arrayRandom[i]} \t");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine(" Array of user values: ");
+            for (int i = 0; i < arrayUser.Length; i++)
+            {
+                Console.Write($"{arrayUser[i]} \t");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine(" Array of sum values: ");
+            for (int i = 0; i < arraySum.Length; i++)
+            {
+                Console.Write($"{arraySum[i]} \t");
+            }
+        }
+    }
+}

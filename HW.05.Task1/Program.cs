20e942b5661ee557a782b37903a44ed2b845de7e
Дарья Норко <d.i.norko@mail.ru>
2021-10-15 14:07:04 +0300

    Add Task2

--- HW.05.Task1/Program.cs
+++ HW.05.Task1/Program.cs
@@ -6,25 +6,27 @@ namespace HW._05.Task1
     {
         static void Main(string[] args)
         {
-            int [] arrayRandom = new int [10];
+            Console.WriteLine(" Let's create an array! Input the length of the array.");
+            int[] arrayUser = new int[int.Parse(Console.ReadLine())];
+
+            Console.WriteLine($"Please, input {arrayUser.Length} values.");
+            Console.WriteLine();
+
+            for (int i = 0; i < arrayUser.Length; i++)
+            {
+                Console.Write($" Value number {i + 1} - ");
+                int num = int.Parse(Console.ReadLine());
+                arrayUser[i] = num;
+            }
+            Console.WriteLine();
+
+            int [] arrayRandom = new int [arrayUser.Length];
             Random rand = new Random();
             for (int i = 0; i<arrayRandom.Length; i++)
             {
                 arrayRandom[i] = rand.Next(0, 200);
             }
-
-            int[] arrayUser = new int[arrayRandom.Length];
-            Console.WriteLine($"Please, input {arrayUser.Length} values.");
-            Console.WriteLine();
-            
-            for (int i = 0; i<arrayUser.Length; i++)
-            {
-                Console.Write($" Value number {i+1} - ");
-               int num = int.Parse(Console.ReadLine());
-                arrayUser[i] = num;
-            }
-            Console.WriteLine();
-
+          
             int[] arraySum = new int[arrayUser.Length];
             for (int i = 0; i < arraySum.Length; i++)
             {
---
+++ HW.05.Task2/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace HW._05.Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            Console.WriteLine(" Input the length of the array ");
+            int[] nums = new int[int.Parse(Console.ReadLine())];
+
+            Console.WriteLine($"Please, input {nums.Length-1} values.");
+            Console.WriteLine();
+
+            for (int i = 0; i < nums.Length-1; i++)
+            {
+                Console.Write($" Value number {i + 1} - ");
+                int num = int.Parse(Console.ReadLine());
+                nums[i] = num;
+            }
+            Console.WriteLine();
+
+            Console.WriteLine(" OK. This is your array: ");
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Console.Write($"{nums[i]} \t");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine(" Please, input one more value.");
+            int addNum = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(" What is the position of this number in the array? ");
+            int indexAddNum = int.Parse(Console.ReadLine());
+
+            while (indexAddNum>nums.Length-1)
+            {
+                Console.WriteLine($" You must choose position from 0 to {nums.Length-1}. " +
+                    $"Please, try again.");
+                indexAddNum = int.Parse(Console.ReadLine());
+            }
+
+            if (indexAddNum == nums.Length - 1)
+            {
+                nums[indexAddNum] = addNum;
+            }
+            else
+            {
+                for (int i = nums.Length-1; i > indexAddNum; i--)
+                {
+                    nums[i] = nums[i - 1];
+                }
+                nums[indexAddNum] = addNum;
+            }
+
+            Console.WriteLine("Good! Here's your new array!");
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Console.Write($"{nums[i]} \t");
+            }
+        }
+    }
+}

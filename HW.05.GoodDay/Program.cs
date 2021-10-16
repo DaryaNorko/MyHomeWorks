ec8452191d3e44cd2c8bd0a64968324b2f1dc82a
Дарья Норко <d.i.norko@mail.ru>
2021-10-16 22:25:07 +0300

    Add all tasks.

---
+++ HW.05.GoodDay/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace HW._05.GoodDay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Вариант решения с оператором switch я добавила прямо в практическую, когда получилось все отправить в Git.
+            //Но там я не заметила, что нельзя указывать каждый час. Здесь другой вариант решения.
+
+            TimeSpan time = DateTime.Now.TimeOfDay;
+
+            switch (time.Hours)
+            {
+                case >=9 when time.Hours < 12:
+                    Console.WriteLine("Good morning, guys!");
+                    break;
+                case >=12 when time.Hours < 15:
+                    Console.WriteLine("Good day, guys!");
+                    break;
+                case >= 15 when time.Hours < 22:
+                    Console.WriteLine("Good evening, guys!");
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}

a22b61efea7ec89438e43708e2839edc6054d9e7
Дарья Норко <d.i.norko@mail.ru>
2021-10-31 18:37:02 +0300

    add task 1

---
+++ HW.08.Task1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+
+namespace HW._08.Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            string symbolString = "1a!2.3!!.. 4.!.?6 7! ..?";
+            int index = symbolString.IndexOf('?');
+
+            StringBuilder part1SB = new StringBuilder(symbolString);
+            StringBuilder part2SB = new StringBuilder(symbolString);
+         
+
+            part1SB.Remove(index+1, part1SB.Length -1-index).Replace('!', '.');
+            char[] part1Array = part1SB.ToString().ToCharArray();
+            part1SB.Clear();
+
+            for (int i = 0; i < part1Array.Length; i++)
+            {
+                if (part1Array[i] != '.')
+                    part1SB.Append(part1Array[i]);
+            }
+            
+            part1SB.Append(part2SB.Remove(0, index+1).Replace(' ', '_'));
+            symbolString = part1SB.ToString();
+
+            Console.WriteLine(symbolString);
+        }
+    }
+}

69a7a6efef3a2d4bfb9429d6f54741d7d883df41
Дарья Норко <d.i.norko@mail.ru>
2021-12-25 19:17:27 +0300

    Add LINQ

--- HW.08.Task1/Program.cs
+++ HW.08.Task1/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace HW._08.Task1
@@ -6,29 +8,19 @@ namespace HW._08.Task1
     class Program
     {
         static void Main(string[] args)
-        {
-           
+        {          
             string symbolString = "1a!2.3!!.. 4.!.?6 7! ..?";
-            int index = symbolString.IndexOf('?');
+            int indexSym = symbolString.IndexOf('?');
 
-            StringBuilder part1SB = new StringBuilder(symbolString);
-            StringBuilder part2SB = new StringBuilder(symbolString);
-         
+            char[] symbolArray = symbolString.ToCharArray();
 
-            part1SB.Remove(index+1, part1SB.Length -1-index).Replace('!', '.');
-            char[] part1Array = part1SB.ToString().ToCharArray();
-            part1SB.Clear();
-
-            for (int i = 0; i < part1Array.Length; i++)
-            {
-                if (part1Array[i] != '.')
-                    part1SB.Append(part1Array[i]);
-            }
+            string part1 = new(symbolArray.TakeWhile(sym => sym != '?').Where(sym => sym != '!' && sym != '.').ToArray());
             
-            part1SB.Append(part2SB.Remove(0, index+1).Replace(' ', '_'));
-            symbolString = part1SB.ToString();
+            string part2 = new(symbolArray.Skip(indexSym).ToArray());
 
-            Console.WriteLine(symbolString);
+            part2 = part2.Replace(' ', '_');
+
+            Console.WriteLine(part1+part2);
         }
     }
 }
